9812609e351726dea9dd3a125e01a204f6ac6765
JosipRados01 <109460688+JosipRados01@users.noreply.github.com>
2022-07-18 00:39:28 +0200

    Bez animacija

--- js.js
+++ js.js
@@ -1,1 +1,15 @@
 console.log("connected")
+let notification = document.getElementById("notification")
+
+function kopiraj(){
+    navigator.clipboard.writeText("josiprados.biz@gmail.com");
+    notification.classList.add("show")
+    setTimeout(() => {
+        notification.classList.remove("show")
+    }, 2000)
+}
+
+function skrollaj(ime){
+    let el = document.getElementById(ime);
+    el.scrollIntoView({behavior: "smooth"});
+}
