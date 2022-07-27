759eff9f3408632e30e06cbfae1407190124be72
JosipRados01 <109460688+JosipRados01@users.noreply.github.com>
2022-07-27 15:44:01 +0200

    dodane animacije i css za big screen

--- js.js
+++ js.js
@@ -3,9 +3,9 @@ let notification = document.getElementById("notification")
 
 function kopiraj(){
     navigator.clipboard.writeText("josiprados.biz@gmail.com");
-    notification.classList.add("show")
+    notification.classList.add("showup")
     setTimeout(() => {
-        notification.classList.remove("show")
+        notification.classList.remove("showup")
     }, 2000)
 }
 
@@ -13,3 +13,22 @@ function skrollaj(ime){
     let el = document.getElementById(ime);
     el.scrollIntoView({behavior: "smooth"});
 }
+
+
+
+// fade in animacije
+let elementi = document.querySelectorAll(".fader")
+
+const observer = new IntersectionObserver( entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+        if (entry.isIntersecting) {observer.unobserve(entry.target)}
+    })
+},{
+    threshold : 0.2,
+    rootMargin: "-30px"
+})
+
+elementi.forEach(element => {
+    observer.observe(element)
+})
